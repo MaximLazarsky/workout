f2fbc092793107b9f5d29ec9e2d1ab3063587fbf
Maxim Lazarskiy <lazarskiy@gmail.com>
2020-11-03 17:17:48 +0200

    TODO refactoring all front and use sagas

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -7,3 +7,12 @@ export const fetchLoginUser = async ({email, password}) => {
     })
     return response.data
 }
+
+export const fetchGetUserData = async() => {
+    const response = await axios.get('http://localhost:5000/api/auth/', {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('Authorization')}`
+        }
+    })
+    return response.data
+}
